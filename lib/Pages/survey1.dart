98d147408f92d69b55f332a1f58da096428d7ff7
trippegre3n <trippegre3n@gmail.com>
2025-01-08 21:17:39 +0000

    fhtrfy

--- lib/Pages/survey1.dart
+++ lib/Pages/survey1.dart
@@ -1,45 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class WorkoutSurveyPage extends StatefulWidget {
-  const WorkoutSurveyPage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _WorkoutSurveyPageState extends State<WorkoutSurveyPage> {
-  int _currentQuestionIndex = 0;
-  List<Map<String, dynamic>> _questions = [
-    {
-      'questionText': 'What is your fitness goal?',
-      'answers': [
-        {'text': 'Lose Weight', 'score': 1},
-        {'text': 'Gain Muscle', 'score': 2},
-        {'text': 'Improve Endurance', 'score': 3},
-        {'text': 'General Fitness', 'score': 4},
-      ],
-    },
-    {
-      'questionText': 'How many days a week do you want to work out?',
-      'answers': [
-        {'text': '1-2 days', 'score': 1},
-        {'text': '3-4 days', 'score': 2},
-        {'text': '5-6 days', 'score': 3},
-        {'text': '7 days', 'score': 4},
-      ],
-    },
-    // Add more questions here...
-  ];
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
 
-  void _answerQuestion(int score) {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<SurveyPageView> createState() => _SurveyPageViewState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentQuestionIndex = (_currentQuestionIndex + 1) % _questions.length;
+      _pageController.nextPage(
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeInOutCubic,
+      );
     });
   }
 
@@ -61,19 +86,41 @@ class _WorkoutSurveyPageState extends State<WorkoutSurveyPage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: _currentQuestionIndex < _questions.length
-          ? Column(
-              children: [
-                Text(_questions[_currentQuestionIndex]['questionText']),
-                ...(_questions[_currentQuestionIndex]['answers'] as List<Map<String, dynamic>>)
-                    .map((answer) => ElevatedButton(
-                          onPressed: () => _answerQuestion(answer['score']),
-                          child: Text(answer['text']),
-                        ))
-                    .toList(),
-              ],
-            )
-          : const Center(child: Text('Survey completed!')),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-// ignore: depend_on_referenced_packages
-import 'package:table_calendar/table_calendar.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -22,72 +21,108 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
+class SurveyPageView extends StatefulWidget {
+  const SurveyPageView({super.key});
+
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  // This will store activities for each day.
-  final Map<DateTime, List<String>> _activities = {};
-  DateTime _selectedDay = DateTime.now();
-  TextEditingController _activityController = TextEditingController();
 
-  // Helper function to add an activity to a specific day
-  void _addActivity(DateTime day, String activity) {
+class _SurveyPageViewState extends State<SurveyPageView> {
+  final PageController _pageController = PageController();
+  int _currentQuestionIndex = 0;
+  int _hoverCounter = 0;
+
+
+  final List<Map<String, dynamic>> _questions = [
+    {
+      'questionText': 'What is your fitness goal?',
+      'answers': [
+        {'text': 'Lose Weight', 'score': 1},
+        {'text': 'Gain Muscle', 'score': 2},
+        {'text': 'Improve Endurance', 'score': 3},
+        {'text': 'General Fitness', 'score': 4},
+      ],
+    },
+    {
+      'questionText': 'How many days a week do you want to work out?',
+      'answers': [
+        {'text': '1-2 days', 'score': 1},
+        {'text': '3-4 days', 'score': 2},
+        {'text': '5-6 days', 'score': 3},
+        {'text': '7 days', 'score': 4},
+      ],
+    },
+  ];
+
+
+  void _answerQuestion(int score) {
     setState(() {
-      if (_activities[day] == null) {
-        _activities[day] = [];
-      }
-      _activities[day]!.add(activity);
+      _currentQuestionIndex = (_currentQuestionIndex + 1) % _questions.length;
+      _pageController.nextPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
     });
   }
 
-  // Helper function to remove an activity from a specific day
-  void _removeActivity(DateTime day, String activity) {
+
+  void _incrementHoverCounter() {
     setState(() {
-      _activities[day]?.remove(activity);
+      _hoverCounter++;
     });
   }
 
-  // Displaying a dialog to add a new activity
-  void _showAddActivityDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text("Add Activity"),
-          content: TextField(
-            controller: _activityController,
-            decoration: const InputDecoration(hintText: 'Enter activity'),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-                if (_activityController.text.isNotEmpty) {
-                  _addActivity(_selectedDay, _activityController.text);
-                }
-                _activityController.clear();
-              },
-              child: const Text('Add'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-                _activityController.clear();
-              },
-              child: const Text('Cancel'),
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      controller: _pageController,
+      scrollDirection: Axis.vertical,
+      itemCount: _questions.length + 1, // Add 1 for the "Survey Completed" page
+      itemBuilder: (context, index) {
+        if (index < _questions.length) {
+          return SurveyQuestion(
+            questionText: _questions[index]['questionText'],
+            answers: _questions[index]['answers'],
+            onAnswerSelected: _answerQuestion,
+            hoverCounter: _hoverCounter,
+            onHover: _incrementHoverCounter,
+          );
+        } else {
+          return const Center(
+            child: Text(
+              'Survey Completed!',
+              style: TextStyle(fontSize: 20),
             ),
           ],
         );
       },
     );
   }
+}
+
+
+class SurveyQuestion extends StatelessWidget {
+  final String questionText;
+  final List<Map<String, dynamic>> answers;
+  final void Function(int) onAnswerSelected;
+  final int hoverCounter;
+  final VoidCallback onHover;
+
+
+  const SurveyQuestion({
+    super.key,
+    required this.questionText,
+    required this.answers,
+    required this.onAnswerSelected,
+    required this.hoverCounter,
+    required this.onHover,
+  });
+
 
   @override
   Widget build(BuildContext context) {
@@ -98,45 +133,41 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Column(
         children: [
-          // Table Calendar widget
-          TableCalendar(
-            firstDay: DateTime.utc(2020, 1, 1),
-            lastDay: DateTime.utc(2030, 12, 31),
-            focusedDay: _selectedDay,
-            selectedDayPredicate: (day) => isSameDay(day, _selectedDay),
-            onDaySelected: (selectedDay, focusedDay) {
-              setState(() {
-                _selectedDay = selectedDay;
-              });
-            },
+          Text(
+            questionText,
+            style: Theme.of(context).textTheme.titleLarge,
           ),
-
-          // List of activities for the selected day
-          Expanded(
-            child: ListView.builder(
-              itemCount: _activities[_selectedDay]?.length ?? 0,
-              itemBuilder: (context, index) {
-                final activity = _activities[_selectedDay]![index];
-                return Dismissible(
-                  key: Key(activity),
-                  direction: DismissDirection.endToStart,
-                  onDismissed: (direction) {
-                    _removeActivity(_selectedDay, activity);
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('$activity removed')),
-                    );
-                  },
-                  background: Container(
-                    color: Colors.red,
-                    child: const Icon(Icons.delete, color: Colors.white),
-                    alignment: Alignment.centerRight,
-                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                  ),
-                  child: ListTile(
-                    title: Text(activity),
-                  ),
-                );
-              },
+          const SizedBox(height: 20),
+          ...answers.map(
+            (answer) => ElevatedButton(
+              onPressed: () => onAnswerSelected(answer['score']),
+              child: Text(answer['text']),
+            ),
+          ),
+          const SizedBox(height: 40),
+          Center(
+            child: MouseRegion(
+              onEnter: (_) => onHover(),
+              child: Container(
+                width: 200,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  'Hover over me!',
+                  style: const TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Center(
+            child: Text(
+              'Hover Count: $hoverCounter',
+              style: Theme.of(context).textTheme.titleMedium,
             ),
           ),
         ],
@@ -148,3 +179,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
